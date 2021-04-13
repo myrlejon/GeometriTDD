5dc42db26ce1716c44175172bd125891635fdd8c
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-04-13 12:18:33 +0200

    Add project files.

---
+++ GeometriTDD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeometriTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sq = new Triangle(6);
+            Console.WriteLine($"Triangels area: {sq.OmkretsLiksig()}");
+        }
+    }
+}
